c9eed033b7572e44ff21c886e5fd1e1b5b20f771
mayank-del <mkjam.007@gmail.com>
2022-10-01 16:51:59 +0530

    Video call app

---
+++ index.js
@@ -0,0 +1,35 @@
+const app=require("express")();
+const server=require("http").createServer(app);
+const cors=require("cors");
+
+const io=require("socket.io")(server,{
+    cors:{
+        origin:'*',
+        methods:["GET","POST"]
+    }
+});
+
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+io.on('connection',(socket)=>{
+    socket.emit('me',socket.id);
+
+    socket.on("disconnect",()=>{
+        socket.broadcast.emit("callEnded")
+    })
+
+    socket.on("callUser",({userToCall,signalData,from,name})=>{
+        io.to(userToCall).emit("callUser",{signal:signalData,from,name})
+    })
+
+    socket.on("answerCall",(data)=>{
+        io.to(data.to).emit("callAccepted",data.signal)
+    })
+})
+
+server.listen(5000,()=>{
+    console.log("server is listeing to 5000")
+})
